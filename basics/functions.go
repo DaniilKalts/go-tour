646326e5122a64e6b7a91b248f27c6bff2b4708a
DaniilKalts <kaltsdaniil@gmail.com>
2025-03-02 09:12:20 +0500

    fix: rename unexported functions to start with lowercase

--- basics/functions.go
+++ basics/functions.go
@@ -7,15 +7,15 @@ package basics
 import "fmt"
 
 func ExampleFunctions() {
-	fmt.Printf("The sum of %d and %d is %d", 10, 20, Add(10, 20))
+	fmt.Printf("The sum of %d and %d is %d", 10, 20, add(10, 20))
 	fmt.Println()
 	fmt.Println()
 
-	fmt.Printf("The factorial of %d is %d", 5, Factorial(5))
+	fmt.Printf("The factorial of %d is %d", 5, factorial(5))
 	fmt.Println()
 	fmt.Println()
 
-	result, err := Divide(5.0, 0.0)
+	result, err := divide(5.0, 0.0)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 	} else {
@@ -24,18 +24,18 @@ func ExampleFunctions() {
 	fmt.Println()
 }
 
-func Add(a, b int) int {
+func add(a, b int) int {
 	return a + b
 }
 
-func Factorial(n int) int {
+func factorial(n int) int {
 	if n <= 1 {
 		return 1
 	}
-	return n * Factorial(n-1)
+	return n * factorial(n-1)
 }
 
-func Divide(dividend, divisor float64) (float64, error) {
+func divide(dividend, divisor float64) (float64, error) {
 	fmt.Printf("Try divide %f by %f...\n", dividend, divisor)
 	if divisor == 0 {
 		return 0, fmt.Errorf("cannot divide by zero!")
